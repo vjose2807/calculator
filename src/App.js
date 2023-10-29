fff25c7a7970f4dfa44997139e8e0503b0371b8c
vjose2807 <vjose2807@gmail.com>
2023-10-29 15:30:00 -0400

    fix

--- src/App.js
+++ src/App.js
@@ -26,7 +26,10 @@ const App = () => {
     setInput((prevInput) => prevInput + value);
   };
 
-  
+  const handleDelete = () => {
+    setInput((prevInput) => prevInput.slice(0, -1));
+  };
+
 
   const handleCalculate = () => {
     try {
@@ -50,23 +53,24 @@ const App = () => {
         {input || '0'}
       </div>
       <div className="buttons">
-        <button id="zero" onClick={() => handleButtonClick('0')}>0</button>
-        <button id="one" onClick={() => handleButtonClick('1')}>1</button>
-        <button id="two" onClick={() => handleButtonClick('2')}>2</button>
-        <button id="three" onClick={() => handleButtonClick('3')}>3</button>
-        <button id="four" onClick={() => handleButtonClick('4')}>4</button>
-        <button id="five" onClick={() => handleButtonClick('5')}>5</button>
-        <button id="six" onClick={() => handleButtonClick('6')}>6</button>
+      <button id="clear" onClick={handleClear}>AC</button>
+        <button id="divide" onClick={() => handleButtonClick('/')}>/</button>
+        <button id="multiply" onClick={() => handleButtonClick('*')}>*</button>
+        <button id="subtract" onClick={() => handleButtonClick('-')}>-</button>
         <button id="seven" onClick={() => handleButtonClick('7')}>7</button>
         <button id="eight" onClick={() => handleButtonClick('8')}>8</button>
         <button id="nine" onClick={() => handleButtonClick('9')}>9</button>
         <button id="add" onClick={() => handleButtonClick('+')}>+</button>
-        <button id="subtract" onClick={() => handleButtonClick('-')}>-</button>
-        <button id="multiply" onClick={() => handleButtonClick('*')}>*</button>
-        <button id="divide" onClick={() => handleButtonClick('/')}>/</button>
-        <button id="decimal" onClick={() => handleButtonClick('.')}>.</button>
+        <button id="four" onClick={() => handleButtonClick('4')}>4</button>
+        <button id="five" onClick={() => handleButtonClick('5')}>5</button>
+        <button id="six" onClick={() => handleButtonClick('6')}>6</button>
+        <button id="delete" onClick={handleDelete}>←</button>
+        <button id="one" onClick={() => handleButtonClick('1')}>1</button>
+        <button id="two" onClick={() => handleButtonClick('2')}>2</button>
+        <button id="three" onClick={() => handleButtonClick('3')}>3</button>
         <button id="equals" onClick={handleCalculate}>=</button>
-        <button id="clear" onClick={handleClear}>AC</button>
+        <button id="decimal" onClick={() => handleButtonClick('.')}>.</button>
+        <button id="zero" onClick={() => handleButtonClick('0')}>0</button>
       </div>
     </div>
   );
